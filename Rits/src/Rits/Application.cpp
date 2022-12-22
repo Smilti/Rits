9194da3ed8c158acc6ff7db7090359a0d5018dbd
Halvard Johannes Nygård <smiltien@gmail.com>
2022-12-22 15:24:52 +0100

    Window Events

--- Rits/src/Rits/Application.cpp
+++ Rits/src/Rits/Application.cpp
@@ -1,20 +1,30 @@
 #include "rtpch.h"
 #include "Application.h"
 
-#include "Rits/Events/ApplicationEvent.h"
-
 #include <GLFW/glfw3.h>
 
 namespace Rits {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
     Application::Application()
     {
         m_Window = std::unique_ptr<Window>(Window::Create());
+        m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
     }
 
     Application::~Application()
     {
     }
+
+    void Application::OnEvent(Event& e)
+    {
+        EventDispatcher dispatcher(e);
+        dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+        RT_CORE_TRACE("{0}", e);
+    }
+
 
     void Application::Run()
     {
@@ -26,4 +36,9 @@ namespace Rits {
         }
     }
 
+    bool Application::OnWindowClose(WindowCloseEvent& e)
+    {
+        m_Running = false;
+        return true;
+    }
 }
